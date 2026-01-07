20c4f0f92a2f6c4216036ac203f6ebf91d45c75d
Danille Hamilton <danillehamilton@Danilles-MacBook-Air.local>
2026-01-07 23:28:05 +0000

    Add case study section ton homepage

--- LA4K-P/src/App.jsx
+++ LA4K-P/src/App.jsx
@@ -5,12 +5,14 @@ import './App.css'
 
 import Hero from "../src/components/hero.jsx";
 import OurStory from "../src/components/OurStory.jsx";
+import CaseStudies from './components/CaseStudies.jsx';
 
 function App() {
   return (
     <>
       <Hero />
       <OurStory />
+      <CaseStudies />
     </>
   );
 }
