9488ef809b8fd6fd2d3e2e0bfcb95ce7aa6cd57d
Danille Hamilton <danillehamilton@Danilles-MacBook-Air.local>
2026-01-07 22:09:29 +0000

    Create hero section

---
+++ LA4K-P/src/components/hero.jsx
@@ -0,0 +1,24 @@
+// src/components/sections/Hero/Hero.jsx
+import "../styles/Hero.modules.css"
+
+export default function Hero() {
+  return (
+    <section className="hero">
+      
+        
+        <div className="hero-text">
+           <h1>
+            LA4K
+          </h1>
+        </div>
+
+        {/* <div className="hero-visual">
+          <div className="hero-image-placeholder">
+            image will go here later 
+          </div>
+        </div>*/}
+
+      
+    </section>
+  );
+}
