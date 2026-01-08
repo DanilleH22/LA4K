486fa0b34241a0dc2135d540f354a244bd753b9c
Danille Hamilton <danillehamilton@Danilles-MacBook-Air.local>
2026-01-08 00:26:48 +0000

    Create moving foot section

---
+++ LA4K-P/src/components/MovingFooter.jsx
@@ -0,0 +1,13 @@
+
+
+export default function MovingFooter() {
+  return (
+    <footer className="moving-footer">
+      <div className="moving-footer-inner">
+        <h1>LA4K</h1>
+        <p>Font here.</p>
+        <h1>LA4K</h1>
+      </div>
+    </footer>
+  );
+}
