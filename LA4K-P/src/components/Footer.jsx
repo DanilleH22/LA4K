a98606952189f2889dfaa3f2fada28a5de5627ce
Danille Hamilton <danillehamilton@Danilles-MacBook-Air.local>
2026-01-08 00:24:32 +0000

    Create footer content

---
+++ LA4K-P/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+export default function OurPackages() {
+  return (
+    <section className="footer">  
+      <div className="footer-inner">
+        <h1>LA4K</h1>
+        <p>Your tagline or description here.</p>
+        <p>&copy; 2023 Your Company. All rights reserved.</p>
+        <ul>
+          <li>Package 1</li>
+          <li>Package 2</li>
+          <li>Package 3</li>
+        </ul>
+      </div>
+    </section>
+  )
+}
