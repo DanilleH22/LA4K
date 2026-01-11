f6a019d729aa26cce8cfee136d4ae254c5adf8ff
Danille Hamilton <danillehamilton@Danilles-MacBook-Air.local>
2026-01-11 23:25:41 +0000

    Add all social links and remove twitter

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -16,23 +16,15 @@ const SocialLinks = () => {
     {
       id: 'facebook',
       name: 'Facebook',
-      url: 'https://facebook.com/',
+      url: 'https://www.facebook.com/share/1Z1uWdRtHj/',
       icon: 'fab fa-facebook-f',
       color: '#1877F2',
       hoverColor: '#0D5FD9'
     },
     {
-      id: 'twitter',
-      name: 'Twitter',
-      url: 'https://twitter.com/',
-      icon: 'fab fa-twitter',
-      color: '#1DA1F2',
-      hoverColor: '#0C85D0'
-    },
-    {
       id: 'instagram',
       name: 'Instagram',
-      url: 'https://instagram.com/',
+      url: 'https://www.instagram.com/mrla4k?igsh=MTFjYnJ0ZGVwMWcxeQ==',
       icon: 'fab fa-instagram',
       color: '#E4405F',
       hoverColor: '#C13584'
@@ -40,7 +32,7 @@ const SocialLinks = () => {
     {
       id: 'youtube',
       name: 'YouTube',
-      url: 'https://youtube.com/',
+      url: 'https://youtube.com/@kinla4k?si=vcpFMuCbxxBBo7Tt',
       icon: 'fab fa-youtube',
       color: '#FF0000',
       hoverColor: '#CC0000'
@@ -48,7 +40,7 @@ const SocialLinks = () => {
     {
       id: 'tiktok',
       name: 'TikTok',
-      url: 'https://tiktok.com/',
+      url: 'https://www.tiktok.com/@lockedaltitude?_r=1&_t=ZN-92ytf49doiE',
       icon: 'fab fa-tiktok',
       color: '#550a50ff',
       hoverColor: '#FF0050'
