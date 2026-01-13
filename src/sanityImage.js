05ad383488529b0f63fa46269bd26ef88809467d
Danille Hamilton <danillehamilton@Danilles-MacBook-Air.local>
2026-01-13 23:10:18 +0000

    Add sanity image part

---
+++ src/sanityImage.js
@@ -0,0 +1,10 @@
+// src/sanityImage.js
+import { createImageUrlBuilder } from '@sanity/image-url';
+import { client } from './sanity/client';
+
+const builder = createImageUrlBuilder(client);
+
+export function urlFor(source) {
+  return builder.image(source);
+}
+
