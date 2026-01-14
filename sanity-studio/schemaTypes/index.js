d47652e0554fd57fed04bdc3879316b679b5ce45
Danille Hamilton <danillehamilton@Danilles-MacBook-Air.local>
2026-01-14 05:16:17 +0000

    Udate index for other pages sanity

--- sanity-studio/schemaTypes/index.js
+++ sanity-studio/schemaTypes/index.js
@@ -1,4 +1,7 @@
 import about from "./about";
 import contact from "./contactUs";
+import home from "./home";
+import footer from "./footer";
+// import Socials from  "./socials";
 
-export const schemaTypes = [about, contact];
+export const schemaTypes = [about, contact, home, footer];
