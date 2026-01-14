1658ae29ef76dbc16826b6f2509f6d7633b5aae9
Danille Hamilton <danillehamilton@Danilles-MacBook-Air.local>
2026-01-14 09:44:07 +0000

    Add new hero - title screen

--- sanity-studio/schemaTypes/index.js
+++ sanity-studio/schemaTypes/index.js
@@ -2,6 +2,7 @@ import about from "./about";
 import contact from "./contactUs";
 import home from "./home";
 import footer from "./footer";
+import titleScreen from "./titleScreen";
 // import Socials from  "./socials";
 
-export const schemaTypes = [about, contact, home, footer];
+export const schemaTypes = [about, contact, home, footer, titleScreen];
