5529615590c3e482bab0f8fc8e966568b6c1c4b0
Danille Hamilton <danillehamilton@Danilles-MacBook-Air.local>
2026-01-14 05:17:40 +0000

    Update sanoty

---
+++ sanity-studio/schemaTypes/footer.js
@@ -0,0 +1,19 @@
+export default {
+  name: "footer",
+  title: "Footer",
+  type: "document",
+  fields: [
+
+    {
+      name: "footerHeading",
+      title: "Footer Heading",
+      type: "string",
+    },
+    {
+      name: "footerText",
+      title: "Footer Text",
+      type: "text",
+    }
+    
+  ],
+}
---
+++ sanity-studio/schemaTypes/socials.js
@@ -0,0 +1,17 @@
+export default {
+  name: "socials",
+  title: "Social Media Links",
+  type: "document",
+  fields: [
+    {
+      name: "platform",
+      title: "Platform",
+      type: "string",
+    },
+    {
+      name: "url",
+      title: "URL",
+      type: "url",
+    },
+  ],
+};
