1e079b3e845b5adb509cfdd59b0f7eb3b69d0d37
Danille Hamilton <danillehamilton@Danilles-MacBook-Air.local>
2026-01-14 05:16:57 +0000

    Update to include sanity

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,9 +2,9 @@
 
 
 
-
+import React, {useState, useEffect} from "react";
 import OurStory from "../components/OurStory.jsx"
-
+import { client } from "../sanity/client";
 import CaseStudies from '../components/CaseStudies.jsx';
 import Services from '../components/Services.jsx';
 // import PlayReel from './components/PlayReel.jsx';
@@ -18,19 +18,55 @@ import SocialLinks from '../components/SocialLinks.jsx';
 import Hero2 from '../components/Hero2.jsx';
 
 
+
 function Homepage() {
+
+   const [home, setHome] = useState(null);
+
+  useEffect(() => {
+    client
+      .fetch(`
+        *[_type == "home"][0]{
+          hero,
+          ourStory,
+          caseStudy {
+  caseStudyHeading,
+  caseStudyBody,
+  caseStudyMedia[] {
+    asset->{
+      url
+    }
+  }
+},
+
+          trustedFeedback,
+          faq,
+          servicesSection,
+          videoReelSection
+        }
+      `)
+      .then((data) => {
+        console.log("HOME DATA:", data);
+        setHome(data);
+      });
+  }, []);
+
+  if (!home) {
+    return <div style={{ color: "white", padding: "2rem" }}>Loading homepage…</div>;
+  }
+
   return (
     <>
 
       <Hero2 />
       <MovingFooter />
-      <OurStory />
+      <OurStory  data={home.ourStory} />
       <SocialLinks />
-      <CaseStudies />
-      <TrustedFeedback />
-      <PhotoReel />
-      <Services />
-      <QA />
+      <CaseStudies  data={home.caseStudy} />
+      <TrustedFeedback  data={home.trustedFeedback} />
+      <PhotoReel  data={home.videoReelSection} />
+      <Services  data={home.servicesSection} />
+      <QA data={home.faq} />
       <Footer />
       <SocialLinks />
       <MovingFooter />
